09a04a27719450c0d74ce0021c1e8656656d523e
Edson B Junior <ebjr.tux@gmail.com>
2021-10-12 20:01:28 -0300

    Configurado o backend do projeto

---
+++ Backend/wichstream/src/main/java/com/projetointegrador/wichstream/WichstreamApplication.java
@@ -0,0 +1,13 @@
+package com.projetointegrador.wichstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WichstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WichstreamApplication.class, args);
+	}
+
+}
---
+++ Backend/wichstream/src/main/java/com/projetointegrador/wichstream/model/Usuario.java
@@ -0,0 +1,56 @@
+package com.projetointegrador.wichstream.model;
+
+public class Usuario {
+	
+	protected Integer id;
+	protected String nome;
+	protected String sobrenome;
+	protected String email;
+	protected String senha;
+	
+	public Usuario() {}
+	
+	public Usuario(Integer id, String nome, String sobrenome, String email, String senha) {
+		this.id = id;
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.email = email;
+		this.senha = senha;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
